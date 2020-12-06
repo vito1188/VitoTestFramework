ffd6eeb800ac4791707ec6d5f5d9bcbd0e8e99d9
Ta, Viet | Vito | SSED <viet.ta@rakuten.com>
2020-12-06 16:06:36 +0900

    init commit

---
+++ VitoTestFramework/VitoTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  VitoTestFramework.h
+//  VitoTestFramework
+//
+//  Created by Ta, Viet | Vito | SSED on 2020/12/06.
+//  Copyright © 2020 VietTa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VitoTestFramework.
+FOUNDATION_EXPORT double VitoTestFrameworkVersionNumber;
+
+//! Project version string for VitoTestFramework.
+FOUNDATION_EXPORT const unsigned char VitoTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VitoTestFramework/PublicHeader.h>
+
+
---
+++ VitoTestFramework/VitoTestViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VitoTestViewController.swift
+//  VitoTestFramework
+//
+//  Created by Ta, Viet | Vito | SSED on 2020/12/06.
+//  Copyright © 2020 VietTa. All rights reserved.
+//
+
+import UIKit
+
+open class VitoTestViewController: UIViewController {
+	public func changeBackground(withColor color: UIColor) {
+		self.view.backgroundColor = color
+	}
+}
